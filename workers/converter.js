1be9ae46765a42c54dde49ef60fc8b27e13bae5d
Julius <juliusvdw@gmail.com>
2020-07-02 12:12:35 +0200

    Created Logic to convert HTML to PDF

---
+++ workers/converter.js
@@ -0,0 +1,10 @@
+//Create logic that will take HTML(data) as input, and convert it into a PDF then return it
+const convert = async (data) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setContent("<h1>Hello World</h1>");
+  await page.pdf({ path: "page.pdf" });
+  await browser.close();
+};
+
+module.exports = convert;
