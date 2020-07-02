7e8639305dd243d80cb336e8c053182bd6069198
Julius <juliusvdw@gmail.com>
2020-07-02 12:25:23 +0200

    Convert controller imported

---
+++ routes/convert.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const convert = require("../controllers/convert");
+
+//Create Route To convert html containing users input to PDF
+router.get("/", convert);
