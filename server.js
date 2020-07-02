91511e6c2fdd82964254c514de559d7b22e85db2
Julius <juliusvdw@gmail.com>
2020-07-02 11:53:16 +0200

    Initiated Middleware

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require("express");
 //Init express app
 const app = express();
 
+//Init Middleware
+app.use(express.json());
+
 //Declare PORT var, fallback to PORT 5000 if environment port is not found
 const PORT = process.env.port || 5000;
 
