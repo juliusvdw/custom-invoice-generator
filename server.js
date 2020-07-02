64eb8b93eb453eba769408290befdad0a08009c7
Julius <juliusvdw@gmail.com>
2020-07-02 11:51:55 +0200

    First commit, srever + directory structure created

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+//Init express app
+const app = express();
+
+//Declare PORT var, fallback to PORT 5000 if environment port is not found
+const PORT = process.env.port || 5000;
+
+//Run server
+app.listen(PORT, console.log(`server listening on port ${PORT} `));
