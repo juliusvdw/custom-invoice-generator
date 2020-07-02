ce0d0a12dc1c0fab1f59e28f981b1b81dc2ffde4
Julius <juliusvdw@gmail.com>
2020-07-02 12:22:17 +0200

    Convert routes and controller logic created

---
+++ controllers.js/convert.js
@@ -0,0 +1,16 @@
+//Bring in converter logic form /workers/converter
+const converter = require("../workers/converter");
+
+//Create Controller to convert HTML to pdf that will be exported to /convert route
+
+exports.convert = async (req, res) => {
+  try {
+    const data = req.body;
+    const pdf = await converter(data);
+
+    res.status(200).sendFile(pdf);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false });
+  }
+};
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //Init Middleware
 app.use(express.json());
 
+//Connect Routes
+app.use("/convert", convert);
+
 //Declare PORT var, fallback to PORT 5000 if environment port is not found
 const PORT = process.env.port || 5000;
 
