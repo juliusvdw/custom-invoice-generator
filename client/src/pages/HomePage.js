d27fe374f4be0857adde0717c8211e5a03360556
Julius <juliusvdw@gmail.com>
2020-07-07 13:20:35 +0200

    Home page layout complete

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -5,20 +5,64 @@ const HomePage = () => {
     <div id="home">
       <div className="container-fluid " style={{ height: "100vh" }}>
         <div className="row h-100">
-          <div className="col-lg-7 col-md-7" id="main-left">
-            Test
+          <div className="col-lg-8 col-md-8 text-left " id="main-left">
+            <div
+              className="container-fluid h-100"
+              style={{
+                paddingTop: "30%",
+                paddingRight: "20%",
+                paddingLeft: "7%",
+              }}
+            >
+              <h2
+                className=""
+                style={{
+                  color: "#1c7ff2",
+                }}
+              >
+                <strong> Create elegant invoices within minutes </strong>
+              </h2>
+              <p style={pStyle} className="mt-3">
+                Select one of our templates and create a stunning invoice within
+                a matter of minutes. For free.
+              </p>
+
+              <btn
+                className="btn btn-outline-primary form-control btn-lg mt-4 w-50 "
+                style={btnStyle}
+              >
+                Create Invoice
+              </btn>
+            </div>
           </div>
           <div
-            className="col-lg-5 col-md-5 d-none d-md-block"
+            className="col-lg-4 col-md-4 d-none d-md-block"
             style={{ background: "#1c7ff2" }}
             id="main-right"
           >
-            Test
+            <img src="files.svg" style={imageStyle} />
           </div>
         </div>
       </div>
     </div>
   );
+};
+
+const imageStyle = {
+  height: "90%",
+  width: "100%",
+  paddingTop: "12%",
+  position: "relative",
+  marginLeft: "-55%",
+};
+
+const pStyle = {
+  fontSize: "1.2rem",
+};
+
+const btnStyle = {
+  borderWidth: "1.5px",
+  height: "50px",
 };
 
 export default HomePage;
