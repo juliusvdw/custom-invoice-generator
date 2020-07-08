992a5d75d651e0651eb59e5f799825ab7a0b5455
Julius <juliusvdw@gmail.com>
2020-07-08 16:23:23 +0200

    Modified + created comments

--- workers/converter.js
+++ workers/converter.js
@@ -1,9 +1,10 @@
 const puppeteer = require("puppeteer");
 
-//Create logic that will take HTML(data) as input, and convert it into a PDF then return it
+//Create logic that will take data as input, and convert it into a PDF then return it
 const converter = async (data) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  //Currently Hard coded html but will be dynamic based on user input
   await page.setContent("<h1>Hello World</h1>");
   const pdf = await page.pdf();
 
