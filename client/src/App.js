9df63b54e07622c5af54233a06d67c7876e6b0ef
Julius <juliusvdw@gmail.com>
2020-07-09 11:22:52 +0200

    Pdf Page imported to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Navbar from "./components/layout/Navbar";
 
 //Bring in pages
 import HomePage from "./components/pages/HomePage";
+import PdfPage from "./components/pages/PdfPage";
 
 function App() {
   return (
