c289fdae45838e487f6eb852c0150d991f1ec707
Julius <juliusvdw@gmail.com>
2020-07-19 12:52:46 +0200

    Form layout created

--- client/src/components/form/Form.js
+++ client/src/components/form/Form.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Form = () => {
-  return <> </>;
+  return <div className="jumbotron" style={jumboStyle}></div>;
+};
+
+const jumboStyle = {
+  height: "60vh",
+  marginTop: "5%",
 };
 
 export default Form;
--- client/src/components/pages/PdfPage.js
+++ client/src/components/pages/PdfPage.js
@@ -1,13 +1,31 @@
 import React, { useEffect } from "react";
 
 import Navbar from "../layout/Navbar";
+import Form from "../form/Form";
 
 const PdfPage = () => {
   return (
     <>
       <Navbar />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-10 mx-auto text-center">
+            <h2 style={headingStyle}>New Invoice</h2>
+          </div>
+        </div>
+
+        <div className="row ">
+          <div className="col-lg-10 mx-auto">
+            <Form />
+          </div>
+        </div>
+      </div>
     </>
   );
+};
+
+const headingStyle = {
+  marginTop: "12%",
 };
 
 export default PdfPage;
