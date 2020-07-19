b5820fb2043c76ddaa8c0c73e524f63f0c0ae21a
Julius <juliusvdw@gmail.com>
2020-07-19 13:11:15 +0200

    Styling + section added

--- client/src/components/form/Form.js
+++ client/src/components/form/Form.js
@@ -1,12 +1,29 @@
 import React from "react";
 
 const Form = () => {
-  return <div className="jumbotron" style={jumboStyle}></div>;
+  return (
+    <div className="jumbotron d-flex" style={jumboStyle}>
+      <div className="col-lg-7 text-left">
+        <h6>From</h6>
+        <textarea
+          className="form-control"
+          placeholder="Your Company or Name&#10; Address"
+          style={textAreaStyle}
+        />
+      </div>
+      <div className="col-lg-4">Test</div>
+    </div>
+  );
 };
 
 const jumboStyle = {
   height: "60vh",
   marginTop: "5%",
+};
+
+const textAreaStyle = {
+  minHeight: "120px",
+  borderRadius: "7px",
 };
 
 export default Form;
