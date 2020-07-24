717e67267600acadf4a3de8d5d549ab2f1361da4
Julius <juliusvdw@gmail.com>
2020-07-24 09:34:30 +0200

    Item component structure created + connected to form

---
+++ client/src/components/form/Item.js
@@ -0,0 +1,60 @@
+import React, { useContext } from "react";
+
+//Bring on context
+import FormContext from "../../context/form/formContext";
+
+const Item = () => {
+  const formContext = useContext(FormContext);
+
+  const { items } = formContext;
+
+  //Create items output list based on item State
+  const itemOutput = items.map((item) => {
+    return (
+      <>
+        <div className="row">
+          <div className="col-lg-6 text-left">
+            <h6>Description</h6>
+            <textarea
+              className="form-control mt-3"
+              style={descriptionStyle}
+              value={item.description}
+            />
+          </div>
+          <div className="col-lg-2 text-left">
+            <h6>Amount</h6>
+            <input
+              type="number"
+              className="form-control mt-3 "
+              placeholder="0.0"
+              value={item.amount}
+            />
+          </div>
+          <div className="col-lg-3 text-left">
+            <h6>Tax</h6>
+            <button className="btn btn-warning  mt-2 form-control">
+              Add Tax
+            </button>
+          </div>
+          <div className="col-lg-1 my-auto text-left">
+            <h4 className="text-danger mt-3">X</h4>
+          </div>
+        </div>
+
+        <button className="btn btn-warning form-control mt-3">
+          Add New Item
+        </button>
+      </>
+    );
+  });
+
+  //Return the Above created itemOutput list that will  be added to the form and be dynamically displayed based on item state
+  return itemOutput;
+};
+
+const descriptionStyle = {
+  minHeight: "40px",
+  borderRadius: "7px",
+};
+
+export default Item;
