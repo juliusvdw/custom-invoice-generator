c4c598f589091a2a6b42342c130f0fbd9ec3ca4d
Julius <juliusvdw@gmail.com>
2020-08-01 09:44:17 +0200

    React-bootstrap tax modal created

---
+++ client/src/components/modals/TaxModal.js
@@ -0,0 +1,48 @@
+import React, { useContext, useState, useEffect } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+const TaxModal = (props) => {
+  return (
+    <Modal
+      {...props}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      animation
+    >
+      <Modal.Header closeButton className=" text-center">
+        <Modal.Title id="contained-modal-title-vcenter text-center">
+          <div className="mx-auto">
+            <h2 className="mx-auto">Add Tax</h2>
+          </div>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body
+        style={{ minHeight: "150px", marginTop: "15px" }}
+        className="mx-auto "
+      >
+        <input
+          style={inputStyle}
+          type="number"
+          placeholder="Tax percentage"
+          className="form-control mb-4"
+          value={taxAmount}
+          name="taxAmount"
+          onChange={(e) => setTax(e.target.value)}
+        />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          type="submit"
+          className="form-control w-90"
+          style={{ height: "50px" }}
+        >
+          <strong>Add tax</strong>
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default TaxModal;
