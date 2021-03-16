2f2e145573eb2b847e31b8711699e874c14bb035
Julius <juliusvdw@gmail.com>
2021-03-16 10:31:25 +0200

    Removed hard coded html + put async func in trycatch

--- workers/converter.js
+++ workers/converter.js
@@ -3,17 +3,21 @@ const compile = require("./compileTemplate");
 
 //Create logic that will take data as input, and convert it into a PDF then return it
 const converter = async (data) => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-  const html = await compile("invoice-1", data);
-  //Currently Hard coded html but will be dynamic based on user input
-  await page.setContent(html);
-  const pdf = await page.pdf();
+    //Compile template with handlebars and passed through data
+    const html = await compile("invoice-1", data);
+    await page.setContent(html);
+    const pdf = await page.pdf();
 
-  console.log("PDF generated");
-  await browser.close();
-  return pdf;
+    console.log("PDF generated");
+    await browser.close();
+    return pdf;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = converter;
