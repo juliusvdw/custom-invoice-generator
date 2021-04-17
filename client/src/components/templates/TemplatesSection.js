baca30b23fb05c28bac4cc74488d6a057f0353ff
Julius <juliusvdw@gmail.com>
2021-04-17 12:39:55 +0200

    SelectTemplate logic removed and added to template cmponent

--- client/src/components/templates/TemplatesSection.js
+++ client/src/components/templates/TemplatesSection.js
@@ -1,18 +1,7 @@
 import React, { useContext } from "react";
-import FormContext from "../../context/form/formContext";
 import Template from "./Template";
 
 const TemplateSection = () => {
-  //Access for context
-  const formContext = useContext(FormContext);
-  //Destructure whats needed from form context
-  const { selectTemplate } = formContext;
-
-  //Handle select template
-  const handleSelect = (id) => {
-    selectTemplate(id);
-  };
-
   return (
     <>
       <h2>Choose a template</h2>
@@ -21,21 +10,9 @@ const TemplateSection = () => {
         style={templatesContainerStyle}
       >
         <div className="row">
-          <Template
-            src={"invoicetest.png"}
-            id={"invoice1"}
-            onClick={() => handleSelect("invoice1")}
-          />
-          <Template
-            src={"invoicetest2.png"}
-            id={"invoice2"}
-            onClick={() => handleSelect("invoice1")}
-          />
-          <Template
-            src={"invoicetest3.png"}
-            id={"invoice3"}
-            onClick={() => handleSelect("invoice1")}
-          />
+          <Template src={"invoicetest.png"} id={"invoice1"} />
+          <Template src={"invoicetest2.png"} id={"invoice2"} />
+          <Template src={"invoicetest3.png"} id={"invoice3"} />
         </div>
       </div>
     </>
